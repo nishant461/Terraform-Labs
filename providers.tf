c4ee314338b58dc943e7b060fd0269f8f0c424ff
Kavita Singh <kavitasingh@Kavitas-MacBook-Air.local>
2025-10-05 07:55:47 +0530

    adding spn details

--- providers.tf
+++ providers.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 provider "azurerm" {
-  tenant_id       = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  subscription_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  client_id       = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  client_secret   = "YOUR_SECRET_HERE"
+  tenant_id       = "91407618-9c60-4397-bdb6-b3607361e938"
+  subscription_id = "e68b40bf-5f8d-4484-b9f3-2da3d7e23167"
+  client_id       = "f2a61b92-d0da-4951-a768-5b00f7d896ce"
+  client_secret   = "9Cv8Q~jtXNI6m14n2uVLBYTy~wD83xHJ9rPbucu3"
   features {}
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,7 @@
-rg_name       = "myResourceGroup"
-location      = "East US"
-vm_base_name  = "myLinuxVM"
+rg_name       = "Infra-Testing"
+location      = "West India"
+vm_base_name  = "LinuxRunner"
 vm_count      = 3
 vm_size       = "Standard_B1s"
-admin_username = "azureuser"
-admin_password = "YourSecurePassword123!"
+admin_username = "nishant5564"
+admin_password = "Nishant@123!"
